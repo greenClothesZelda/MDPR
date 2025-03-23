b0b6a586b3047878fc5008c69598a562ac392a51
Blummerhen <doo379928@gmail.com>
2025-03-23 21:06:33 +0900

    Update ref.py
    self.Q_past 업데이트 코드 self.device로 받게 수정

--- ref.py
+++ ref.py
@@ -152,7 +152,7 @@ class Reference:
             final_passages.add(idx)  # 남은 개수만큼 main_ref에서 추가
 
         # 🔹 Q_past 및 QA_list 업데이트
-        self.Q_past = torch.cat([self.Q_past, embedded_query.detach().cpu()], dim=0)
+        self.Q_past = torch.cat([self.Q_past, embedded_query.detach().to(self.device)], dim=0)
         new_QA = torch.tensor([[idx, -1, -1] for idx in main_ref[:1]], dtype=torch.long)  # 첫 번째 main_ref만 저장
         self.QA_list = torch.cat([self.QA_list, new_QA], dim=0)
 
