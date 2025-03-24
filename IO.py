f6a98c123c4e070235329253225db24ebd036210
김진수 <100560249+greenClothesZelda@users.noreply.github.com>
2025-03-24 23:31:38 +0900

    임시

--- IO.py
+++ IO.py
@@ -1,32 +1,23 @@
 import pandas as pd
 import json
 from datasets import load_dataset
-def load_passages(passages_file):
-    df = pd.read_csv(passages_file, sep='\t', header=0, dtype={'id': str}, nrows=10)
 
-    return [
-        {"id": row['id'], "text": row['text'], "title": row['title']}
-        for _, row in df.iterrows()
-    ]
+def load_passages_in_chunks(passages_file, chunk_size):
+    chunks = pd.read_csv(passages_file, sep='\t', header=0, dtype={'id': str}, chunksize=chunk_size)
+
+    for chunk in chunks:
+        passages = [
+            {"id": row['id'], "text": row['text'], "title": row['title']}
+            for _, row in chunk.iterrows()
+        ]
+        yield passages  # 제너레이터로 반환, 한 번에 chunk_size개씩 처리 가능
+
 
 def read_jsonl_to_list(file_path):
     # 파일에서 JSON 데이터 읽기
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    # for item in data:
-    #     print(f"Dataset: {item['dataset']}")
-    #     print(f"Question: {item['question']}")
-    #     print("Answers:", item['answers'])
-    #     print("Positive Contexts:")
-    #     # 수정된 코드 (txt 키 확인)
-    #     print(item['positive_ctxs'])
-    #     print("Negative Contexts:")
-    #     print(type(item['negative_ctxs']))
-    #     print('hard_negative_ctxs')
-    #     print((item['hard_negative_ctxs'][0]['score']))
-    #     print("\n")
-    #     break
     return data
 class PassageLoader:
     def __init__(self, passages_file):
--- result.py
+++ result.py
@@ -33,7 +33,6 @@ def main(question):
         raise ValueError("❌ Error: `set_ref()` must be called before using `main()`.")  # ✅ 방어 코드 추가
 
     all_references = r.get_reference(question, 9) # ✅ 하나의 리스트만 반환
-    print(all_references)
     result_texts = [r.passage_texts[idx] for idx in all_references]
     return result_texts
 
